362b2c36cb6b263f4629a89504b376fafd861741
Nate <natan.vrl@protonmail.com>
2024-11-30 22:12:59 +0100

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Header } from "@/src/components/layout/Header";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
   weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  display: "swap",
+  variable: "--font-alpino",
 });
 
 export const metadata: Metadata = {
@@ -24,11 +21,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" className={alpino.className}>
+      <body className="">
+        <Header />
+        <main>{children}</main>
       </body>
     </html>
   );
---
+++ src/components/section/Hero.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Bounded } from "../ui/Bounded";
+import Image from "next/image";
+
+export default function Hero(): JSX.Element {
+  return (
+    <Bounded>
+      <div className="grid">
+        <div className="grid h-screen place-items-center">
+          <h1 className="hero-header text-7xl font-black uppercase leading-[.8] text-orange-500 md:text-[9rem] lg:text-[13rem]">
+            Live Gutsy
+          </h1>
+          <p className="hero-subheading mt-12 text-5xl font-semibold text-sky-950 lg:text-6xl">
+            Soda Perfected
+          </p>
+          <p  className="hero-body text-2xl font-normal text-sky-950">3-5g sugar, 9g fiber, 5 delicious flavors</p>
+          <Button className="hero-button mt-12">shop now</Button>
+        </div>
+
+        <div className="text-side relative z-[80] grid h-screen items-center gap-4 md:grid-cols-2">
+          <Image
+            src="/assets/all-cans-bunched.png"
+            alt="all cans hero img"
+            width={400}
+            height={400}
+          />
+          <h2>Try all five flavors</h2>
+          <p>
+            Our soda is made with real fruit juice and a touch of cane sugar. We
+            never use artificial sweeteners or high fructose corn syrup. Try all
+            five flavors and find your favorite!
+          </p>
+        </div>
+      </div>
+    </Bounded>
+  );
+}
