e64e66442ab4af4a89918d013ccf0220875947dd
Nate <natan.vrl@protonmail.com>
2024-12-10 21:06:12 +0100

    working on animations w gsap

--- src/components/section/Hero.tsx
+++ src/components/section/Hero.tsx
@@ -1,20 +1,46 @@
-import React from "react";
+"use client";
+
 import { Bounded } from "../ui/Bounded";
 import Image from "next/image";
+import { TextSplitter } from "../ui/TextSplitter";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 
 export default function Hero(): JSX.Element {
+  useGSAP(() => {
+    const introTl = gsap.timeline();
+
+    introTl.from(".hero-header-word", {
+      scale: 3,
+      opacity: 0,
+    })
+  });
+
   return (
-    <Bounded>
+    <Bounded className="hero">
       <div className="grid">
         <div className="grid h-screen place-items-center">
-          <h1 className="hero-header text-7xl font-black uppercase leading-[.8] text-orange-500 md:text-[9rem] lg:text-[13rem]">
-            Live Gutsy
+          <h1 className="hero-header text-center text-7xl font-black uppercase leading-[.8] text-orange-500 md:text-[9rem] lg:text-[13rem]">
+            <TextSplitter
+              text="Live gutsy"
+              wordDisplayStyle="block"
+              className="hero-header-word"
+            />
           </h1>
           <p className="hero-subheading mt-12 text-5xl font-semibold text-sky-950 lg:text-6xl">
             Soda Perfected
           </p>
-          <p  className="hero-body text-2xl font-normal text-sky-950">3-5g sugar, 9g fiber, 5 delicious flavors</p>
-          <button className="main-btn-style hero-button mt-12" aria-label="hero button to shop now">shop now</button>
+          <p className="hero-body text-2xl font-normal text-sky-950">
+            3-5g sugar, 9g fiber, 5 delicious flavors
+          </p>
+          <button
+            className="main-btn-style hero-button mt-12"
+            aria-label="hero button to shop now"
+          >
+            shop now
+          </button>
         </div>
 
         <div className="text-side relative z-[80] grid h-screen items-center gap-4 md:grid-cols-2">
@@ -23,13 +49,18 @@ export default function Hero(): JSX.Element {
             alt="all cans hero img"
             width={400}
             height={400}
+            className="w-full md:hidden"
           />
-          <h2>Try all five flavors</h2>
-          <p>
-            Our soda is made with real fruit juice and a touch of cane sugar. We
-            never use artificial sweeteners or high fructose corn syrup. Try all
-            five flavors and find your favorite!
-          </p>
+          <div>
+            <h2 className="text-side-heading text-balance text-6xl font-black uppercase text-sky-950 lg:text-8xl">
+              <TextSplitter text="Try all five flavors" />
+            </h2>
+            <p className="text-side-body mt-4 max-w-xl text-balance text-xl font-normal text-sky-950">
+              Our soda is made with real fruit juice and a touch of cane sugar.
+              We never use artificial sweeteners or high fructose corn syrup.
+              Try all five flavors and find your favorite!
+            </p>
+          </div>
         </div>
       </div>
     </Bounded>
